a5b047e489415e9ed3b88d81abefb12fee949bfd
ruojunm <mengruojun@gmail.com>
2018-05-25 21:01:01 +0800

    first test commit

---
+++ data_strcture/Test.py
@@ -0,0 +1,22 @@
+def swap(lyst, i, j):
+    """Exchange the times"""
+    temp = lyst[i]
+    lyst[i] = lyst[j]
+    lyst[j] = temp
+
+
+def selectionSort(lyst):
+    i = 0
+    while i < len(lyst) - 1:
+        minIndex = i
+        j = i + 1
+        while j < len(lyst):
+            if (lyst[j] < lyst[minIndex]):
+                minIndex = j
+            j += 1
+        if minIndex != i:
+            swap(lyst, minIndex, i)
+        i += 1
+
+if __name__ == '__main__':
+    print(selectionSort(list(range(99,1,-2))))
