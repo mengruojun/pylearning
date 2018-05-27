e18e06968e212b53d36bb3fed1686af8734f7cc7
ruojunm <mengruojun@gmail.com>
2018-05-27 12:26:42 +0800

    add log and time stat for Prime.py

--- data_strcture/Prime.py
+++ data_strcture/Prime.py
@@ -1,3 +1,8 @@
+import logging
+import time
+import sys
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
 def getPrimeNumber(limit=None):
     i = 2
     prime_list = [2]
@@ -18,8 +23,19 @@ def getPrimeNumber(limit=None):
             """not break, then it is a prime, add it to the prime_list"""
             prime_list.append(i)
     
-    print(prime_list)
+    logging.debug(prime_list)
+    return prime_list
     
 
 if __name__ == '__main__':
-    getPrimeNumber(1000000)
+    start = time.time()
+    logging.info("Start")
+    if(len(sys.argv) == 1): limit = 100
+    else: 
+        limit = int(sys.argv[1])
+
+    prime_list = getPrimeNumber(limit)
+    end = time.time()
+    logging.info("End")
+    logging.info("Finished in " + str(end - start) + " Seconds")
+    logging.info("Max Prime in rang is " + str(prime_list[-1]))
